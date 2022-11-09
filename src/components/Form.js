b6cb997ac2168110c0472ab3dc6c29f192c72be7
nordeeen <ahmadnrdnsah@gmail.com>
2022-11-09 23:20:48 +0700

    add reusable component in Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,77 @@
 import React from 'react';
 import Header from './Header';
 
+const textForm = [
+  {
+    id: 1,
+    title: 'Logo Perusahaan',
+    placeholder: 'Ketikan Logo Perusahaan',
+    link: 'Dalam bentuk link dan url',
+  },
+  {
+    id: 2,
+    title: 'Nama Perusahaan',
+    placeholder: 'Ketikan Nama Perusahaan',
+  },
+  {
+    id: 3,
+    title: 'Nama Lowongan',
+    placeholder: 'Ketikan Nama Lowongan',
+  },
+  {
+    id: 4,
+    title: 'Status Karyawan',
+    placeholder: 'Ketikan Status Karyawan',
+  },
+  {
+    id: 5,
+    title: 'Kisaran Gaji Karyawan',
+    textCenter: 'sampai dengan',
+    placeholderOne: '1000.000',
+    placeholderTwo: '10.000.000',
+  },
+  {
+    id: 6,
+    title: 'Tanggal Posting',
+    placeholder: 'dd/mm/yyyy',
+  },
+];
+
+const InputForm = (props) => {
+  return (
+    <div className="flex flex-col">
+      <label className="text-sm font-normal text-left mb-2">{props.title}</label>
+      <input
+        type="text"
+        placeholder={props.placeholder}
+        className="w-[600px] px-1 py-1 border-2 border-gray-400 rounded-[4px]"
+      />
+      <p className="text-xs text-gray-400 font-normal">{props.link}</p>
+    </div>
+  );
+};
+
+const InputSalary = (props) => {
+  return (
+    <div className="flex flex-col">
+      <label className="text-sm font-normal text-left mb-2">{props.title}</label>
+      <div className="flex">
+        <input
+          type="number"
+          placeholder={props.placeholderOne}
+          className="w-[225px] px-1 py-1 border-2 border-gray-400 rounded-[4px]"
+        />
+        <p className="mx-auto">{props.textCenter}</p>
+        <input
+          type="number"
+          placeholder={props.placeholderTwo}
+          className="w-[225px] px-1 py-1 border-2 border-gray-400 rounded-[4px]"
+        />
+      </div>
+    </div>
+  );
+};
+
 const Form = () => {
   return (
     <>
@@ -11,63 +82,28 @@ const Form = () => {
             Buat Lowongan :
           </h1>
           <form>
-            <div className="flex flex-col">
-              <label className="text-sm font-normal text-left mb-2">Logo Perusahaan</label>
-              <input
-                type="text"
-                placeholder="Ketikan Logo Perusahaan"
-                className="w-[600px] px-1 py-1 border-2 border-gray-400 rounded-[4px]"
-              />
-              <p className="text-xs text-gray-400 font-normal">Dalam bentuk link url</p>
-            </div>
-            <div className="flex flex-col">
-              <label className="text-sm font-normal text-left mb-2">Nama Perusahaan</label>
-              <input
-                type="text"
-                placeholder="Ketikan Nama Perusahaan"
-                className="w-[600px] px-1 py-1 border-2 border-gray-400 rounded-[4px]"
-              />
-            </div>
-            <div className="flex flex-col">
-              <label className="text-sm font-normal text-left mb-2">Nama Lowongan</label>
-              <input
-                type="text"
-                placeholder="Ketikan Nama Lowongan"
-                className="w-[600px] px-1 py-1 border-2 border-gray-400 rounded-[4px]"
-              />
-            </div>
-            <div className="flex flex-col">
-              <label className="text-sm font-normal text-left mb-2">Status Karyawan</label>
-              <input
-                type="text"
-                placeholder="Ketikan Status Perusahaan"
-                className="w-[600px] px-1 py-1 border-2 border-gray-400 rounded-[4px]"
-              />
-            </div>
-            <div className="flex flex-col">
-              <label className="text-sm font-normal text-left mb-2">Kisaran Gaji Karyawan</label>
-              <div className="flex">
-                <input
-                  type="number"
-                  placeholder="1000.000"
-                  className="w-[225px] px-1 py-1 border-2 border-gray-400 rounded-[4px]"
-                />
-                <p className="mx-auto">sampai dengan</p>
-                <input
-                  type="number"
-                  placeholder="10.000.00"
-                  className="w-[225px] px-1 py-1 border-2 border-gray-400 rounded-[4px]"
-                />
-              </div>
-            </div>
-            <div className="flex flex-col">
-              <label className="text-sm font-normal text-left mb-2">Tanggal Posting</label>
-              <input
-                type="date"
-                placeholder="dd/mm/yy"
-                className="w-[600px] px-1 py-1 border-2 border-gray-400 rounded-[4px]"
-              />
-            </div>
+            {textForm.map((val) => {
+              if (val.id === 5) {
+                return (
+                  <InputSalary
+                    key={val.id}
+                    title={val.title}
+                    placeholderOne={val.placeholderOne}
+                    placeholderTwo={val.placeholderTwo}
+                    textCenter={val.textCenter}
+                  />
+                );
+              } else {
+                return (
+                  <InputForm
+                    key={val.id}
+                    title={val.title}
+                    link={val.link}
+                    placeholder={val.placeholder}
+                  />
+                );
+              }
+            })}
             <button className="bg-blue-600 text-white text-xs px-2 py-2 rounded-md mt-2 uppercase">
               simpan
             </button>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/form');
+  };
+
   return (
     <>
       <header className="w-full h-auto bg-blue-200 flex justify-between items-center py-4 px-6">
         <h1 className="text-4xl text-red-600 font-semibold text-center">Klob</h1>
-        <a href="#/" className="text-blue-600 underline">
+        <a href="#/" onClick={handleClick} className="text-blue-600 underline">
           Buat Lowongan
         </a>
       </header>
